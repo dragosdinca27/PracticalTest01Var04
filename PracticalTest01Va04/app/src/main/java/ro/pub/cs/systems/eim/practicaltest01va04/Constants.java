0426f768c07df6b55556f94671d4136a463dbc3d
Dragos-Ionut DINCA <dragos.dinca@zitec.com>
2021-04-15 09:57:03 +0300

    add B2

--- PracticalTest01Va04/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01va04/Constants.java
+++ PracticalTest01Va04/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01va04/Constants.java
@@ -1,4 +1,7 @@
 package ro.pub.cs.systems.eim.practicaltest01va04;
 
 public class Constants {
+
+    public static final String NAME_TEXT = "nameEditText";
+    public static final String GROUP_TEXT = "groupEditText" ;
 }
--- PracticalTest01Va04/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01va04/PracticalTest01Var0MainActivity.java
+++ PracticalTest01Va04/app/src/main/java/ro/pub/cs/systems/eim/practicaltest01va04/PracticalTest01Var0MainActivity.java
@@ -8,6 +8,7 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.SyncStateContract;
 import android.view.View;
 
 import android.view.Menu;
@@ -21,13 +22,15 @@ import android.widget.Toast;
 
 public class PracticalTest01Var0MainActivity extends AppCompatActivity {
 
+    private EditText  nameEditText, groupEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_practical_test01_var04_main);
 
-        EditText nameEditText = (EditText) findViewById(R.id.name_edit_text);
-        EditText groupEditText = (EditText) findViewById(R.id.group_edit_text);
+        nameEditText = (EditText) findViewById(R.id.name_edit_text);
+        groupEditText = (EditText) findViewById(R.id.group_edit_text);
 
         CheckBox nameCheckBox = (CheckBox) findViewById(R.id.name_check_box);
         CheckBox groupCheckBox = (CheckBox) findViewById(R.id.group_check_box);
@@ -59,27 +62,43 @@ public class PracticalTest01Var0MainActivity extends AppCompatActivity {
                 }}
             }
         });
+
+        if (savedInstanceState != null) {
+            if (savedInstanceState.containsKey(Constants.NAME_TEXT)) {
+                nameEditText.setText(savedInstanceState.getString(Constants.NAME_TEXT));
+            } else {
+                nameEditText.setText(String.valueOf("Studentul Perfect"));
+            }
+            if (savedInstanceState.containsKey(Constants.GROUP_TEXT)) {
+                groupEditText.setText(savedInstanceState.getString(Constants.GROUP_TEXT));
+            } else {
+                groupEditText.setText(String.valueOf("343C4"));
+            }
+        } else {
+            nameEditText.setText(String.valueOf("Studentul Perfect"));
+            groupEditText.setText(String.valueOf("343C4"));
+        }
     }
-//
-//    @Override
-//    protected void onSaveInstanceState(Bundle savedInstanceState) {
-//        super.onSaveInstanceState(savedInstanceState);
-//        savedInstanceState.putString(Constants.LEFT_COUNT, nameEditText.getText().toString());
-//        savedInstanceState.putString(Constants.RIGHT_COUNT, rightEditText.getText().toString());
-//    }
-//
-//    @Override
-//    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-//        if (savedInstanceState.containsKey(Constants.LEFT_COUNT)) {
-//            nameEditText.setText(savedInstanceState.getString(Constants.LEFT_COUNT));
-//        } else {
-//            nameEditText.setText(String.valueOf(0));
-//        }
-//        if (savedInstanceState.containsKey(Constants.RIGHT_COUNT)) {
-//            groupEditText.setText(savedInstanceState.getString(Constants.RIGHT_COUNT));
-//        } else {
-//            groupEditText.setText(String.valueOf(0));
-//        }
-//    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle savedInstanceState) {
+        super.onSaveInstanceState(savedInstanceState);
+        savedInstanceState.putString(Constants.NAME_TEXT, nameEditText.getText().toString());
+        savedInstanceState.putString(Constants.GROUP_TEXT, groupEditText.getText().toString());
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        if (savedInstanceState.containsKey(Constants.NAME_TEXT)) {
+            nameEditText.setText(savedInstanceState.getString(Constants.NAME_TEXT));
+        } else {
+            nameEditText.setText(String.valueOf("Studentul Perfect"));
+        }
+        if (savedInstanceState.containsKey(Constants.GROUP_TEXT)) {
+            groupEditText.setText(savedInstanceState.getString(Constants.GROUP_TEXT));
+        } else {
+            groupEditText.setText(String.valueOf("343C4"));
+        }
+    }
 
 }
